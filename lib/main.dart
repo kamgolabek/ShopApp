1ccfe1164d1a5a6f3ce47ca3ff7a30b1a5dc1e81
Kamil Gołąbek <kamgolabek@gmail.com>
2020-03-07 12:47:55 +0100

    providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 import './screens/products_overview_screen.dart';
-import 'package:google_fonts/google_fonts.dart';
+import './providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        // textTheme: GoogleFonts.shadowsIntoLightTextTheme(
-        //   Theme.of(context).textTheme,
-        // ),
-        // primaryTextTheme: GoogleFonts.shadowsIntoLightTextTheme(
-        //   Theme.of(context).textTheme,
-        // )
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          // textTheme: GoogleFonts.shadowsIntoLightTextTheme(
+          //   Theme.of(context).textTheme,
+          // ),
+          // primaryTextTheme: GoogleFonts.shadowsIntoLightTextTheme(
+          //   Theme.of(context).textTheme,
+          // )
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
     );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/providers/product.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem(
-    this.id,
-    this.title,
-    this.imageUrl,
-  );
-
+ 
   @override
   Widget build(BuildContext context) {
+
+    final product = Provider.of<Product>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
         ),
         footer: GridTileBar(
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(product.isFavorite? Icons.favorite: Icons.favorite_border),
+            onPressed: () {product.toggleFavorite();},
             color: Theme.of(context).accentColor,
           ),
           trailing: IconButton(
@@ -32,7 +37,7 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor),
           backgroundColor: Colors.black87,
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
             // style: Theme.of(context).textTheme.bodyText1,
           ),
